5a93c122fc2c3021ad1278ccd8f822a63a33154c
romdoniahmad185-cmyk <romdoniahmad185@gmail.com>
2026-08-13 15:22:49 +0700

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,43 @@
+const CACHE_NAME = "creator-viewer-v1";
+
+const FILES_TO_CACHE = [
+    "./",
+    "./index.html",
+    "./icon-192.png",
+    "./icon-512.png",
+    "./manifest.json"
+];
+
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                return cache.addAll(FILES_TO_CACHE);
+            })
+            .then(() => {
+                return self.skipWaiting();
+            })
+            .catch(error => {
+                console.error(
+                    "Creator Viewer cache gagal:",
+                    error
+                );
+                throw error;
+            })
+    );
+});
+
+self.addEventListener("activate", event => {
+    event.waitUntil(
+        self.clients.claim()
+    );
+});
+
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        fetch(event.request)
+            .catch(() => {
+                return caches.match(event.request);
+            })
+    );
+});
